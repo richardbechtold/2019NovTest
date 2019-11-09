3c907fe4a7f8150d494bbbc691f5b4d249835bc1
Richard Bechtold <richardbechtold@AbridgeTech-MBPro2019.local>
2019-11-09 10:36:49 -0500

    2nd line of comment entered from shell command line

---
+++ 19test7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  19test7
+//
+//  Created by Richard Bechtold on 10/18/19.
+//  Copyright © 2019 Richard Bechtold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
